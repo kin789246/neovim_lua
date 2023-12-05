f918de6c3f9720b41d6b3b9cf48eb55d57f554c8
j-ching69 <ocezask@hotmail.com>
2023-12-05 16:00:08 +0800

    kitty: disable TwilioSans calt feature; neovim: keymap gdt as go to definition in new tab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,9 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+  local opts = {buffer = bufnr}
+  local bind = vim.keymap.set
+  bind('n', 'gdt', '<cmd>tab split | lua vim.lsp.buf.definition()<cr>', opts)
 end)
 
 require('mason').setup({})
